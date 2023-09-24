7b98d900912d3a91a5d6f8cbe6e2e405e91eda4c
Admin <823213790@qq.com>
2023-09-24 17:03:34 +0800

    2023.09.24

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
@@ -27,7 +28,16 @@ public class GlobalExceptionHandler {
     }
     @ExceptionHandler
     public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
-        return null;
+        String message = ex.getMessage();
+        if(message.contains("Duplicate entry")){
+            String[] split = message.split(" ");
+            String username = split[2];
+            String msg = username + MessageConstant.ALREADY_EXISTS;
+            return Result.error(msg);
+        }
+        else{
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
     }
 
 }
