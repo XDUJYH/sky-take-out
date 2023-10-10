211a248c7f76a2ba1f76d06722891f647495cee0
Admin <823213790@qq.com>
2023-10-10 11:42:31 +0800

    day 5 end

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,29 @@
+package com.sky.controller.user;
+
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Slf4j
+@Api(tags = "店铺相关接口")
+public class ShopController {
+    public static final String Key = "SHOP_STATUS";
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping("/status")
+    @ApiOperation("获取店铺的营业状态")
+    public Result<Integer> getStatus(){
+        Integer status = (Integer) redisTemplate.opsForValue().get(Key);
+        log.info("获取店铺的营业状态为：{}", status == 1 ?"营业中":"打烊中");
+        return Result.success(status);
+    }
+}
