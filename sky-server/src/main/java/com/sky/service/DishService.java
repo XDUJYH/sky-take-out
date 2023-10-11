ac5b0e8b1ed6180af187f6a201fff438ae5ec9a9
Admin <823213790@qq.com>
2023-10-11 13:56:39 +0800

    day 6 end

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -45,4 +45,11 @@ public interface DishService {
     void startOrStop(Integer status, Long id);
 
     List<Dish> getDishByCategoryId(Long categoryId);
+
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 }
